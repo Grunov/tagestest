53c8c3cb244346f8e95e9f0461ea2e5a77dc8c86
Oleg Grunov <grunov.co@gmail.com>
2021-11-03 04:35:38 +0300

    small refactor

--- index.js
+++ index.js
@@ -13,23 +13,18 @@ async function preprocessData() {
   let users = await DataService.getData('users');
   let posts = await DataService.getData('posts', 100);
 
-  posts = posts.map(post => {
-    const currentPost = {
-      ...post,
-      title_crop: post.title.substr(0, 20) + '...'
-    };
-    return currentPost;
-  });
+  posts = posts.map(post => ({
+    ...post,
+    title_crop: post.title.substr(0, 20) + '...'
+  }));
 
-  users = users.map(user => {
-    return {
-      ...user,
-      address: `${user.address.city}, ${user.address.street}, ${user.address.suite}`,
-      website: `https://${user.website}`,
-      company: user.company.name,
-      posts: posts.filter(post => post.userId === user.id)
-    }
-  });
+  users = users.map(user => ({
+    ...user,
+    address: `${user.address.city}, ${user.address.street}, ${user.address.suite}`,
+    website: `https://${user.website}`,
+    company: user.company.name,
+    posts: posts.filter(post => post.userId === user.id)
+  }));
 
   const user2 = users.find(user => user.id === 2);
 
@@ -41,12 +36,10 @@ async function preprocessData() {
 
   const user2PostsCommentsResults = await Promise.all(user2PostsCommentsPromisses);
 
-  user2.posts = user2.posts.map((post, i) => {
-    return {
-      ...post,
-      comments: user2PostsCommentsResults[i]
-    }
-  });
+  user2.posts = user2.posts.map((post, i) => ({
+    ...post,
+    comments: user2PostsCommentsResults[i]
+  }));
 
   return users;
 }
